829eacce38dc3f406768e067c955227623c5143b
Arsandi Saputra <71245230+arsandisaputra@users.noreply.github.com>
2024-03-05 21:55:42 +0700

    Try Counter

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,9 +1,9 @@
 <div x-data="{ count: 0 }">
     {{-- <h1>{{ $count }}</h1> --}}
 
-    <button x-on:click="count--">Decrement</button>
+    <button x-on:click="count--" class="rounded-lg bg-orange-500 px-4 py-2 text-neutral-100">Decrement</button>
     <span x-text="count"></span>
-    <button x-on:click="count++">Increment</button>
+    <button x-on:click="count++" class="rounded-lg bg-orange-500 px-4 py-2 text-neutral-100">Increment</button>
     {{-- <button wire:click="increment">+</button>
 
     <button wire:click="decrement">-</button> --}}
